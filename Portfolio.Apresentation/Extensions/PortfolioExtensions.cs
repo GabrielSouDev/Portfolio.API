3c39a98d0afaccd654c186eae543b84d5f73a62d
Gabriel Souza <gabriel.calde21@gmail.com>
2026-01-05 14:16:29 -0300

    Implementação de salvamento de imagens

--- Portfolio.Apresentation/Extensions/PortfolioExtensions.cs
+++ Portfolio.Apresentation/Extensions/PortfolioExtensions.cs
@@ -1,7 +1,11 @@
 ﻿using System.Buffers.Text;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
 using Microsoft.AspNetCore.Mvc;
 using Portfolio.API.Models;
 using Portfolio.API.Services;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Portfolio.API.Extensions;
 
@@ -20,21 +24,62 @@ public static class PortfolioExtensions
             return Results.Ok(projects); 
         });
 
-        group.MapPost("/override", ([FromServices] BackedStateService jsonBackedStateService, [FromBody] IEnumerable<ProjectItem> projectItem) =>
+        group.MapPost("/override", async ([FromServices] BackedStateService jsonBackedStateService, [FromBody] IEnumerable<ProjectItem> projectItem) =>
         {
             foreach (var project in projectItem)
             {
-                foreach (var image in project.Images)
+                var images = project.Images.ToList();
+                for (int i = 0; i < images.Count; i++)
                 {
-                    if (Base64.IsValid(image))
+                    if (IsBase64(images[i]))
                     {
-                        var base64 = image;
-                        var path = $"{DateTime.Now}{project.Title}.jpeg";
-                        //...
+                        var base64 = images[i];
+                        var directoryPath = Path.Combine(Environment.CurrentDirectory, "Storage", "Images");
+                        var imageName = $"{Guid.NewGuid()}.jpg";
+                        await SaveImage(directoryPath, imageName, base64);
+
+                        images[i] = Path.Combine(directoryPath, imageName);
                     }
                 }
+
+                project.Images = images;
             }
             jsonBackedStateService.SaveProjects(projectItem);
         }).RequireAuthorization();
+    }
+
+    private static async Task SaveImage(string directoryPath, string imageName, string base64)
+    {
+        if (!Directory.Exists(directoryPath))
+            Directory.CreateDirectory(directoryPath);
+
+
+        var bytes = Convert.FromBase64String(CleanBase64(base64));
+        await File.WriteAllBytesAsync(Path.Combine(directoryPath, imageName), bytes);
+    }
+
+    // Helpers
+    private static string CleanBase64(string input)
+    {
+        var match = Regex.Match(input, @"^data:image\/\w+;base64,(.+)$");
+        return match.Success ? match.Groups[1].Value : input;
+    }
+
+    private static bool IsBase64(string input)
+    {
+        if (string.IsNullOrWhiteSpace(input)) return false;
+
+        var match = Regex.Match(input, @"^data:image\/\w+;base64,(.+)$");
+        if (match.Success) input = match.Groups[1].Value;
+
+        try
+        {
+            Convert.FromBase64String(input);
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
     }
 }
